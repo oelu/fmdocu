7ef85629eccaecc7e9d70ba1221ff24acc8718dc
oli <olivierchenk@gmail.com>
2014-05-13 23:52:24 +0200

    added documentation renamed functions

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Fortimail Configuration Parser
 Parsers a Fortimail Configuration to a dictionary. Prints out a Configuration Report 
 in Markdown Syntax.
+
+## Configuration Sections
+
+### Misc
+* hamode
+
+# Network
+## Network Interfaces
+* Ports
+
+## Routes
+* Routes with default route
+
+# MTA
+## Mail Queues
+## Sessions
+## Domain Configurations
--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python2.7
 """ fmparse.py
 Usage:
-      fmparse.py (-f <file> | --file <file>)
+      fmparse.py [-f <file> | --file <file>]
 
 """
 __author__ = 'olivier'
@@ -17,8 +17,32 @@ def print_markdown_report():
     """
     prints fortimail configuration report in markdown format
     """
-    None
+    return None
 
+def print_markdown(confobj):
+    """
+    prints a markdown representation of a fortiguard
+    configuration object
+    """
+    for elem in confobj:
+        if "config" in elem:
+            print "|" + elem.replace("config", "") + "|"
+        elif "edit" in elem:
+            print "|" + elem.replace("edit", "") + "|"
+        elif "set" in elem:
+            # TODO: has to be splitted in key and value words
+            # with break of |
+            print "|" + elem.replace("set", "") + "|"
+
+def print_routes(configobj)
+    """
+    The routes in fortigate configuration objects
+    need special treatment.
+    The default route contains no destination network
+    such as 0.0.0.0. This needs to be added.
+    """
+    # TODO: implement function
+    return configobj
 
 def main():
     """
@@ -27,6 +51,9 @@ def main():
     # gets arguments from docopt
     arguments = docopt(__doc__)
     configfile = arguments['<file>']
+    # default value for testing
+    if configfile is None:
+        configfile = 'fml.cfg'
 
     # parse configuration file
     parse = CiscoConfParse(configfile)
@@ -36,6 +63,9 @@ def main():
     nics = parse.find_all_children('config system interface')
     ha = parse.find_all_children('config system ha')
     access_profiles = parse.find_all_children('config system accprofile')
+
+    print_markdown(routes)
+
 
 if __name__ == "__main__":
     main()
